a4d28dfb1ad3eac5c49b3f76a09092c4265f8a9a
Abuzar Siddiqui <abuzar.siddiqui@cognizant.com>
2020-02-13 19:24:55 +0530

    Code to handle person details crud operation

---
+++ src/app/app.component.ts
@@ -0,0 +1,97 @@
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormGroup, FormControl, FormBuilder, FormArray } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'person-app';
+  data: FormGroup;
+  deleteData: FormGroup;
+  personData: FormGroup;
+  favArray = [{ item: "Cricket" }, { item: "Football" }, { item: "Tennis" }];
+  url: string;
+  message1: string;
+  message2: string;
+
+  constructor(private fb: FormBuilder, private http: HttpClient) {}
+
+  ngOnInit() {
+    this.createPayload();
+    this.createAddPayload();
+    this.createDeletePayload();
+    this.displayValues();
+    this.url = 'http://localhost:8080/project/rest/person';
+  }
+
+  createPayload() {
+    this.data = new FormGroup({
+      first_name: new FormControl(''),
+      last_name: new FormControl(''),
+      age: new FormControl(''),
+      favourite_color: new FormControl(''),
+      hobby: new FormArray([
+        new FormControl(),
+        new FormControl(),
+        new FormControl()
+      ])
+    });
+  }
+
+  createAddPayload() {
+    this.personData = new FormGroup({
+      first_name: new FormControl(''),
+      last_name: new FormControl(''),
+      age: new FormControl(''),
+      favourite_color: new FormControl(''),
+      hobby: this.fb.array([])
+    });
+  }
+
+  createDeletePayload() {
+    this.deleteData = new FormGroup({
+      first_name: new FormControl(''),
+      last_name: new FormControl('')
+    });
+  }
+
+  addPersonDetails() {
+    console.log(this.personData);
+    this.http.post(this.url, this.personData.value).subscribe(data => {
+      this.message1 = data.toString();
+    });    
+  }
+
+  updatePersonDetails() {
+    console.log(this.personData);
+    const updatedUrl = this.url + "/" + this.personData.get('first_name').value + "/" + this.personData.get('last_name').value;
+    this.http.put(updatedUrl, this.personData.value).subscribe(data => {
+      this.message1 = data.toString();
+    });
+  }
+
+  deletePersonDetails() {
+    console.log(this.deleteData);
+    const updatedUrl = this.url + "/" + this.deleteData.get('first_name').value + "/" + this.deleteData.get('last_name').value;
+    this.http.delete(updatedUrl, this.deleteData.value).subscribe(data => {
+      this.message2 = data.toString();
+    });
+  }
+
+  changeAction(data, event) {
+    const favouritesArray = <FormArray>this.personData.controls.hobby;
+
+    if (event) {
+      favouritesArray.push(new FormControl(data));
+    }
+  }
+
+  displayValues() {
+    this.http.get(this.url).subscribe(response => {
+      this.data.patchValue(response);
+    });
+  }
+}
